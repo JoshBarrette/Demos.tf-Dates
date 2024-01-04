520be0d3ac843ba927dc92565c292654155e55c3
JoshBarrette <joshpbarrette@gmail.com>
2024-01-03 21:36:31 -0600

    Oopsie

--- scripts/content.js
+++ scripts/content.js
@@ -2,9 +2,6 @@ const targetNode = document.body;
 
 function updateDates() {
     const td = document.querySelectorAll("td.date");
-
-    console.log(td);
-
     td.forEach((d) => {
         const date = new Date(d.getAttribute("title"));
         d.textContent = date.toDateString() + ", " + date.toLocaleTimeString('en-US');
