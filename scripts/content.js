f025783cdeba1a154b122ae1ceaadb6a27a08c16
JoshBarrette <joshpbarrette@gmail.com>
2024-01-05 22:53:22 -0600

    Fixed for Firefox

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,19 @@
-const targetNode = document.body;
+const listBody = document.querySelector("tbody");
+
+const config = { childList: true, subtree: true };
+const observer = new MutationObserver(updateDates);
 
 function updateDates() {
+    observer.disconnect();
+
     const td = document.querySelectorAll("td.date");
     td.forEach((d) => {
         const date = new Date(d.getAttribute("title"));
         d.textContent = date.toDateString() + ", " + date.toLocaleTimeString('en-US');
     });
+
+    observer.observe(listBody, config);
 }
 
-const observer = new MutationObserver(updateDates);
-
-const config = { childList: true, subtree: true };
-observer.observe(targetNode, config);
-
 updateDates();
+observer.observe(listBody, config);
